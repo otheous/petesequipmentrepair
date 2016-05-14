00031f264d53c46e69b0f040f64e366eca8049d9
Tim Schultz <timschultz@Tims-MacBook-Pro.local>
2016-05-14 18:45:04 -0500

    Added routing configs

--- app/contact.component.ts
+++ app/contact.component.ts
@@ -1,12 +1,7 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-contact',
-    template: `
-		<div>
-			<h3>Contact Us</h3>
-			<span> <h2>Tim Peterson</h2> </span>
-		</div>
-	`
+    selector: 'petes-contact',
+    templateUrl: 'app/contact.component.html'
 })
 export class ContactComponent { }
